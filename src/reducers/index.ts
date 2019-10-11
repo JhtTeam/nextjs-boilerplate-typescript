1bda7a08c1030d9a6dee8cd0d880afb6ba21997a
The Anh <anhndt@macbook.pro>
2019-10-11 10:46:37 +0700

    remove template

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 /* PLOP_INJECT_REDUCERS_IMPORT */
-import user from './user'
 
 export default combineReducers({
     /* PLOP_INJECT_REDUCERS_INDEX */
-	user,
 })
